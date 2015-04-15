d31e004302156ea5f7fa4bd2ebe7427784f004fd
MorganDawe <mdawe@discoverygarden.ca>
2015-04-15 15:20:36 -0300

    Updated theme to support shibboleth login link.

---
+++ templates/region--sub-header.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the sub_header region.
+ */
+?>
+<?php if ($content): ?>
+  <div class="region-sub-header">
+    <div class="sub-header-region-wrapper">
+      <?php print $content; ?>
+    </div>
+  </div>
+  
+<?php endif; ?>

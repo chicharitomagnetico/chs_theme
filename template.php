1ca910ef053da1aacbbb24e5445bdd3f69119d4c
Brandon Morrissey <bmorrissey95@gmail.com>
2017-05-11 10:28:03 -0300

    Changes based on feedback

--- template.php
+++ template.php
@@ -127,31 +127,34 @@ function chs_theme_preprocess_islandora_basic_collection_grid(&$variables) {
  */
 function chs_theme_preprocess_islandora_solr_metadata_display(&$variables) {
   $elevated = explode(",", theme_get_setting('chs_theme_elevated_collections'));
+
   if (in_array($variables['islandora_object']->id, $elevated)) {
     $variables['theme_hook_suggestions'][] = "islandora_solr_metadata_display__elevated_collections";
+  }
+
+  if (isset($variables['solr_fields']['mods_accessCondition_useAndReproduction_ms'])) {
+    $variables['solr_fields']['mods_accessCondition_useAndReproduction_xlinkhref_ms'] = array();
   }
 }
 
 /**
- * Implements hook_preprocess().
+ * Implements hook_process().
  */
 function chs_theme_process_islandora_solr_metadata_display(&$variables) {
   $solr_fields = $variables['solr_fields'];
 
-  if (isset($solr_fields['mods_accessCondition_useAndReproduction_xlinkhref_ms']) && isset($solr_fields['mods_accessCondition_useAndReproduction_ms'])) {
+  if (isset($solr_fields['mods_accessCondition_useAndReproduction_ms'])) {
     // Take the xlink and node values and create an anchor out of them.
     $link = reset($solr_fields['mods_accessCondition_useAndReproduction_xlinkhref_ms']['value']);
-    $value = reset($solr_fields['mods_accessCondition_useAndReproduction_ms']['value']);
+    $text = reset($solr_fields['mods_accessCondition_useAndReproduction_ms']['value']);
 
-    $anchor = "<a href='$link'>$value</a>";
-
-    if (!empty($anchor)) {
+    if (!empty($text) && !empty($link)) {
       $variables['solr_fields']['mods_accessCondition_useAndReproduction_ms']['value'] = array(
-        0 => $anchor,
+        0 => l($text, $link),
       );
-      // Unset as we don't need this value any longer.
-      unset($variables['solr_fields']['mods_accessCondition_useAndReproduction_xlinkhref_ms']);
     }
+    // Unset value as we no longer need it.
+    unset($variables['solr_fields']['mods_accessCondition_useAndReproduction_xlinkhref_ms']);
   }
 }
 
